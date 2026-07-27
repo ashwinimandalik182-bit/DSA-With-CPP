5262b30520079f96c6bf89eac9eadc3205be5a8c
Ashwini Mandlik <ashwinimandalik182@gmail.com>
2026-07-27 22:35:08 +0530

    ADD CPP Basic Program

---
+++ CPP-Basics/02_PrintName.cpp
@@ -0,0 +1,16 @@
+//Print your name, college, and branch.
+#include<iostream>
+using namespace std;
+int main(){
+    string name, clg, branch;
+    cout<<"Enter the Your Name :";
+    cin>>name;
+    cout<<"Enter Your College Name:";
+    cin>>clg;
+    cout<<"Enter Your College Name:";
+    cin>>branch;
+   cout << "My Name is " << name << endl
+     << "I'm Pursuing My Degree In " << clg << endl
+     << "In The Branch  of " << branch << endl;
+    return 0;
+}
