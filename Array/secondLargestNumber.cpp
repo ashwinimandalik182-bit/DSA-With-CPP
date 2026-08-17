4d71722cc9ab335a51e0161a436f07c2f08656f9
unknown <ashwinimandalik182@gmail.com>
2026-08-17 14:40:43 +0530

    Add secondLargestNumber Program

---
+++ Array/secondLargestNumber.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+
+    int arr[] = {10, 5, 20, 8, 15};
+    int n = 5;
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    cout << "Largest Number = " << largest << endl;
+    cout << "Second Largest Number = " << secondLargest << endl;
+
+    return 0;
+}
