87243c5666a349df3d266a717b7d8369f0a2ec3a
unknown <ashwinimandalik182@gmail.com>
2026-08-21 12:51:44 +0530

    Added New ProgramSearch In Sorted Array

---
+++ Array/SearchInRotatedSortedArray.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int search(vector<int>& nums, int target) {
+
+    int st = 0;
+    int end = nums.size() - 1;
+
+    while (st <= end) {
+
+        int mid = st + (end - st) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        // Left half sorted 
+        if (nums[st] <= nums[mid]) {
+
+            if (nums[st] <= target && target < nums[mid]) {
+                end = mid - 1;
+            }
+            else {
+                st = mid + 1;
+            }
+        }
+
+        // Right half sorted
+        else {
+
+            if (nums[mid] < target && target <= nums[end]) {
+                st = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 7;
+
+    int result = search(nums, target);
+
+    cout << "Target index: " << result << endl;
+
+    return 0;
+}
